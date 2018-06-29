df9e67075343635af7ba4d3d5a9c3cb1d84ffe6c
gitmeysam <niarajimeysam@gmail.com>
2018-06-29 15:31:29 +0430

    test1

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,8 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', 'Published'),
-    )
+    published_ = (('draft', 'Draft'), ('published', 'Published'),)
+    STATUS_CHOICES = published_
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
